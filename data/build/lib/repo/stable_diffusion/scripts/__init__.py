98f1c9c6baa62869a624671ffa61942bcda27d69
ForserX <sv3nk@yandex.ru>
2023-02-06 15:25:58 +0300

    Del: Removed trash

--- data/build/lib/repo/stable_diffusion/scripts/__init__.py
+++
@@ -1,1 +0,0 @@
-from apps.stable_diffusion.scripts.txt2img import txt2img_inf
--- data/build/lib/repo/stable_diffusion/src/schedulers/__init__.py
+++
@@ -1,4 +0,0 @@
-from repo.stable_diffusion.src.schedulers.sd_schedulers import get_schedulers
-from repo.stable_diffusion.src.schedulers.shark_eulerdiscrete import (
-    SharkEulerDiscreteScheduler,
-)
