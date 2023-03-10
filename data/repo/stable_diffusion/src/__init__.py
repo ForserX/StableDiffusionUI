63540cfff1d1776622bb8d6cd69cfabc23223f7c
ForserX <sv3nk@yandex.ru>
2023-03-10 12:45:01 +0300

    Deleted old shark files. Need update to new version

--- data/repo/stable_diffusion/src/__init__.py
+++
@@ -1,8 +0,0 @@
-from repo.stable_diffusion.src.utils import (
-    args,
-    set_init_device_flags,
-    prompt_examples,
-    get_available_devices,
-)
-from repo.stable_diffusion.src.pipelines import Text2ImagePipeline
-from repo.stable_diffusion.src.schedulers import get_schedulers
--- data/repo/stable_diffusion/src/pipelines/__init__.py
+++
@@ -1,3 +0,0 @@
-from repo.stable_diffusion.src.pipelines.pipeline_shark_stable_diffusion_txt2img import (
-    Text2ImagePipeline,
-)
