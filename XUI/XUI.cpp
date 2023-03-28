7951617f18fd4bf1189346621085edc706a50cd2
ForserX <sv3nk@yandex.ru>
2023-03-28 09:45:20 +0300

    Added cmd runner

---
+++ XUI/XUI.cpp
@@ -0,0 +1,25 @@
+﻿// XUI.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <Windows.h>
+#include <filesystem>
+#include <string>
+
+std::string GetTryFileDirectory()
+{
+	char buffer[MAX_PATH];
+	GetModuleFileNameA(NULL, buffer, MAX_PATH);
+	std::string::size_type pos = std::string(buffer).find_last_of("\\/");
+
+	return std::string(buffer).substr(0, pos);
+}
+
+int main()
+{
+	std::string Dir = std::move(GetTryFileDirectory());
+
+	std::filesystem::current_path(Dir); //setting path
+	system("@start bins\\SD-FXUI.exe");
+
+	return 0;
+}
--- ui-src/App.xaml.cs
+++ ui-src/App.xaml.cs
@@ -18,6 +18,6 @@ namespace SD_FXUI
     {
     }
 
-    
-   
+
+
 }
