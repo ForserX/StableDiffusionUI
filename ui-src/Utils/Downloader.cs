7ec1ba5dbf34de928f22c40335186d47845a389d
ForserX <sv3nk@yandex.ru>
2023-05-05 21:44:04 +0300

    Okay, downloader is working now...

--- ui-src/Utils/Downloader.cs
+++ ui-src/Utils/Downloader.cs
@@ -1,105 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Net;
-using System.Reflection.PortableExecutable;
-using System.Runtime.CompilerServices;
-using System.Security.Policy;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows;
-using Downloader;
 
 namespace SD_FXUI.Utils
 {
     public static class FileDownloader
     {
-        private static DownloadConfiguration downloadOpt = null;
-        private static DownloadService downloader = null;
+        private static WebClient Client = new WebClient();
+        private static bool TaskCompleted = false;
         private static float progressStatusNextCheck = 0;
-        public static void InitDownloaderConfig()
+
+        public static void Initial()
         {
-            downloadOpt = new DownloadConfiguration()
-            {
-                // usually, hosts support max to 8000 bytes, default values is 8000
-                BufferBlockSize = 4096,
-                // file parts to download, default value is 1
-                ChunkCount = 8,
-                // download speed limited to 2MB/s, default values is zero or unlimited
-                //MaximumBytesPerSecond = 1024 * 1024 * 2,
-                // the maximum number of times to fail
-                MaxTryAgainOnFailover = 5,
-                // download parts of file as parallel or not. Default value is false
-                ParallelDownload = true,
-                // number of parallel downloads. The default value is the same as the chunk count
-                ParallelCount = 8,
-                // timeout (millisecond) per stream block reader, default values is 1000
-                Timeout = 1000,
-                // set true if you want to download just a specific range of bytes of a large file
-                RangeDownload = false,
-                // floor offset of download range of a large file
-                RangeLow = 0,
-                // ceiling offset of download range of a large file
-                RangeHigh = 0,
-                // clear package chunks data when download completed with failure, default value is false
-                ClearPackageOnCompletionWithFailure = true,
-                // minimum size of chunking to download a file in multiple parts, default value is 512
-                MinimumSizeOfChunking = 1024,
-                // Before starting the download, reserve the storage space of the file as file size, default value is false
-                ReserveStorageSpaceBeforeStartingDownload = true,
-                // config and customize request headers
-                RequestConfiguration =
-                {
-                    Accept = "*/*",
-                    //CookieContainer = cookies,
-                    Headers = new WebHeaderCollection(), // { your custom headers }
-                    KeepAlive = true, // default value is false
-                    ProtocolVersion = HttpVersion.Version11, // default value is HTTP 1.1
-                    UseDefaultCredentials = false,
-                    // your custom user agent or your_app_name/app_version.
-                    UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-
-                }
-            };
-
-            downloader = new DownloadService(downloadOpt);
-
-            //  this.slW.ValueChanged += new System.Windows.RoutedPropertyChangedEventHandler<double>(this.slW_ValueChanged);
-            //  private void slH_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-
-            downloader.DownloadStarted += OnDownloadStarted;
-            //downloader.ChunkDownloadProgressChanged += OnChunkDownloadProgressChanged;
-            downloader.DownloadProgressChanged += OnDownloadProgressChanged;
-            downloader.DownloadFileCompleted += OnDownloadFileCompleted;
-
+            Client.DownloadFileCompleted += OnDownloadFileCompleted;
+            Client.DownloadProgressChanged += OnDownloadProgressChanged;
         }
 
         public static async Task DownloadFileAsync(string url, string FullFilePath)
         {
-            await downloader.DownloadFileTaskAsync(url, FullFilePath);
+            if (!System.IO.File.Exists(url))
+            {
+                TaskCompleted = false;
+                Host.Print("Download started: " + FullFilePath);
+                Client.DownloadFileAsync(new Uri(url), FullFilePath);
+            }
         }
 
-        private static void OnDownloadStarted(object sender, DownloadStartedEventArgs e)
-        {            
-            Host.Print("Download started: " + downloader.Package.FileName + " from Address: " + downloader.Package.Address + " total file size: " + (downloader.Package.TotalFileSize/1024/1024).ToString() + " MB");
-        }
-
-        private static void OnChunkDownloadProgressChanged(object sender, Downloader.DownloadProgressChangedEventArgs e)
+        public static void Wait()
         {
-            //Console.SetCursorPosition(0, Console.CursorTop - 1);
-            //Host.ClearCurrentConsoleLine();            
+            while (!TaskCompleted)
+                Thread.Sleep(10);
         }
 
-        private static void OnDownloadProgressChanged(object sender, Downloader.DownloadProgressChangedEventArgs e)
+        private static void OnDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
         {
             //Console.SetCursorPosition(0, Console.CursorTop - 1);
             //Host.ClearCurrentConsoleLine();
-            if(downloader.Package.SaveProgress >= progressStatusNextCheck + 5.0f)
+            if(e.ProgressPercentage >= progressStatusNextCheck + 5.0f)
             {
                 progressStatusNextCheck += 5.0f;
-                Host.Print("DownloadProgress: " + downloader.Package.SaveProgress.ToString().Substring(0,6) + "%");
+                Host.Print("DownloadProgress: " + e.ProgressPercentage.ToString() + $"% ({e.BytesReceived / 1024 / 1024}/{e.TotalBytesToReceive / 1024 / 1024} mb)" );
             }
             
         }
@@ -107,6 +49,7 @@ namespace SD_FXUI.Utils
         private static void OnDownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
         {
             progressStatusNextCheck = 0;
+            TaskCompleted = true;
             Host.Print("OnDownloadFileCompleted");
         }
 
--- ui-src/Utils/Models/WGetDownloadModels.cs
+++ ui-src/Utils/Models/WGetDownloadModels.cs
@@ -1,4 +1,5 @@
 ﻿using ABI.System;
+using SD_FXUI.Utils;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -10,70 +11,37 @@ namespace SD_FXUI
 {
     internal class WGetDownloadModels
     {
-        public static void DownloadUpscalerONNX()
-        {
-            string WorkingDir = FS.GetModelDir() + "upscaler/onnx/";
-
-            if (Directory.Exists(WorkingDir))
-            {
-                return;
-            }
-
-            Directory.CreateDirectory(WorkingDir);
-            Directory.CreateDirectory(WorkingDir + "low_res_scheduler");
-            Directory.CreateDirectory(WorkingDir + "scheduler");
-            Directory.CreateDirectory(WorkingDir + "text_encoder");
-            Directory.CreateDirectory(WorkingDir + "tokenizer");
-            Directory.CreateDirectory(WorkingDir + "unet");
-            Directory.CreateDirectory(WorkingDir + "vae");
-
-            string WGetFile = "\"" + FS.GetToolsDir() + "wget.exe\" ";
-            Host Download = new Host(WorkingDir);
-            Download.Start();
-            Download.Send(WGetFile + "-O \"low_res_scheduler/scheduler_config.json\" https://huggingface.co/ssube/stable-diffusion-x4-upscaler-onnx/raw/main/low_res_scheduler/scheduler_config.json");
-
-            Download.Send(WGetFile + "-O \"scheduler/scheduler_config.json\" https://huggingface.co/ssube/stable-diffusion-x4-upscaler-onnx/raw/main/scheduler/scheduler_config.json");
-
-            Download.Send(WGetFile + "-O \"text_encoder/model.onnx\" https://huggingface.co/ssube/stable-diffusion-x4-upscaler-onnx/resolve/main/text_encoder/model.onnx");
-
-            Download.Send(WGetFile + "-O \"tokenizer/vocab.json\" https://huggingface.co/ssube/stable-diffusion-x4-upscaler-onnx/raw/main/tokenizer/vocab.json");
-            Download.Send(WGetFile + "-O \"tokenizer/merges.txt\" https://huggingface.co/ssube/stable-diffusion-x4-upscaler-onnx/raw/main/tokenizer/merges.txt");
-            Download.Send(WGetFile + "-O \"tokenizer/special_tokens_map.json\" https://huggingface.co/ssube/stable-diffusion-x4-upscaler-onnx/raw/main/tokenizer/special_tokens_map.json");
-            Download.Send(WGetFile + "-O \"tokenizer/tokenizer_config.json\" https://huggingface.co/ssube/stable-diffusion-x4-upscaler-onnx/raw/main/tokenizer/tokenizer_config.json");
-
-            Download.Send(WGetFile + "-O \"unet/weights.pb\" https://huggingface.co/ssube/stable-diffusion-x4-upscaler-onnx/resolve/main/unet/weights.pb");
-            Download.Send(WGetFile + "-O \"unet/model.onnx\" https://huggingface.co/ssube/stable-diffusion-x4-upscaler-onnx/resolve/main/unet/model.onnx");
-
-            Download.Send(WGetFile + "-O \"vae/model.onnx\" https://huggingface.co/ssube/stable-diffusion-x4-upscaler-onnx/resolve/main/vae/model.onnx");
-            Download.Send(WGetFile + "https://huggingface.co/ssube/stable-diffusion-x4-upscaler-onnx/raw/main/model_index.json");
-        }
         public static void DownloadCNPoser(HelperControlNet.ControlTypes Type)
         {
             string WorkingDir = FS.GetModelDir() + "controlnet/sd-controlnet/";
+
+            FileDownloader.Initial();
 
             Directory.CreateDirectory(WorkingDir);
             Directory.CreateDirectory(WorkingDir + "anannotator");
             Directory.CreateDirectory(WorkingDir + "anannotator\\ckpts");
 
-            string WGetFile = "\"" + FS.GetToolsDir() + "wget.exe\" ";
-            Host Download = new Host(WorkingDir);
-            Download.Start();
-
             if (Type == HelperControlNet.ControlTypes.Poser)
             {
-                Download.Send(WGetFile + "-O \"anannotator\\ckpts\\body_pose_model.pth\" https://huggingface.co/lllyasviel/Annotators/resolve/main/body_pose_model.pth");
-                Download.Send(WGetFile + "-O \"anannotator\\ckpts\\hand_pose_model.pth\" https://huggingface.co/lllyasviel/Annotators/resolve/main/hand_pose_model.pth");
-                Download.Send(WGetFile + "-O \"anannotator\\ckpts\\facenet.pth\" https://huggingface.co/lllyasviel/Annotators/resolve/main/facenet.pth");
+                FileDownloader.DownloadFileAsync("https://huggingface.co/lllyasviel/Annotators/resolve/main/body_pose_model.pth", WorkingDir + @"anannotator/ckpts/body_pose_model.pth");
+                FileDownloader.Wait();
+
+                FileDownloader.DownloadFileAsync("https://huggingface.co/lllyasviel/Annotators/resolve/main/hand_pose_model.pth", WorkingDir + @"anannotator/ckpts/hand_pose_model.pth");
+                FileDownloader.Wait();
+
+                FileDownloader.DownloadFileAsync("https://huggingface.co/lllyasviel/Annotators/resolve/main/facenet.pth", WorkingDir + @"anannotator/ckpts/facenet.pth");
+                FileDownloader.Wait();
             }
 
             if (Type == HelperControlNet.ControlTypes.Hed)
-                Download.Send(WGetFile + "-O \"anannotator\\ckpts\\network-bsds500.pth\" https://huggingface.co/lllyasviel/ControlNet/resolve/main/annotator/ckpts/network-bsds500.pth");
+            {
+                FileDownloader.DownloadFileAsync("https://huggingface.co/lllyasviel/ControlNet/resolve/main/annotator/ckpts/network-bsds500.pth", WorkingDir + @"anannotator/ckpts/network-bsds500.pth");
+            }
 
             if (Type == HelperControlNet.ControlTypes.Mlsd)
-                Download.Send(WGetFile + "-O \"anannotator\\ckpts\\mlsd_large_512_fp32.pth\" https://huggingface.co/lllyasviel/ControlNet/resolve/main/annotator/ckpts/mlsd_large_512_fp32.pth");
-
-            Download.SendExitCommand();
-            Download.Wait();
+            {
+                FileDownloader.DownloadFileAsync("https://huggingface.co/lllyasviel/ControlNet/resolve/main/annotator/ckpts/mlsd_large_512_fp32.pth", WorkingDir + @"anannotator/ckpts/mlsd_large_512_fp32.pth");
+            }
         }
         public static void DownloadSDPoser()
         {
